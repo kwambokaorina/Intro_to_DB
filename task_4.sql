6e0cce261e1db8837303c8183760b9394d0d8e81
Mitchel Kwamboka <130556636+kwambokaorina@users.noreply.github.com>
2024-10-20 01:11:49 -1200

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,1 +1,14 @@
+-- Print full description of the table Books from the current database
+SELECT 
+    column_name AS 'Column',
+    column_type AS 'Type',
+    is_nullable AS 'Nullable',
+    column_key AS 'Key',
+    column_default AS 'Default',
+    extra AS 'Extra'
+FROM 
+    information_schema.columns
+WHERE 
+    table_schema = DATABASE() 
+    AND table_name = 'Books';
 
